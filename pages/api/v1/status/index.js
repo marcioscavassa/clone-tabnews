ff17c15c151f3336ca5157a19e4f321e934ea74f
Marcio Scavassa <121499243+marcioscavassa@users.noreply.github.com>
2023-11-30 06:29:04 +0000

    add `/api/v1/status` endpoint

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,8 @@
+function status(request, response) {
+  response
+    .status(200)
+    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  // .send("teste");
+}
+
+export default status;
